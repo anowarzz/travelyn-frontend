1fca81c291520c0f892cb9155e0c22b6c52164e7
Anowar Hosen <anowarhosen444@gmail.com>
2025-08-11 09:29:56 +0600

    feat(redux): configure redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { baseApi } from "./baseApi";
+// ...
+
+export const store = configureStore({
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+// Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
