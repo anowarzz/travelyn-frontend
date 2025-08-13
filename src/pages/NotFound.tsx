2b1f3a3ca9c0b9a9061a00ffc73be7227e55fd93
Anowar Hosen <anowarhosen444@gmail.com>
2025-08-13 21:41:13 +0600

    fix: update gradient of not found page to match 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
   const location = useLocation();
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-[var(--color-myGreen-200)] via-[var(--color-myRed-100)] to-[var(--color-myGreen-300)] px-2 py-2">
+    <div className="min-h-screen bg-gradient-to-br from-myGreen-950 to-myRed-950 flex items-center justify-center p-6">
       <div className="container mx-auto px-2">
         <div className="max-w-xl mx-auto">
           <div className="p-4 md:p-6 text-center">
@@ -31,10 +31,10 @@ const NotFound = () => {
 
             {/* Message */}
             <div className="mb-6">
-              <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-slate-800 mb-2">
+              <h2 className="text-xl sm:text-2xl md:text-3xl font-bold text-slate-50 mb-2">
                 Oops! This page is not on the Map !
               </h2>
-              <p className=" md:text-lg text-slate-700 max-w-lg mx-auto leading-relaxed">
+              <p className=" md:text-lg text-slate-50 max-w-lg mx-auto leading-relaxed">
                 <br />
                 Don't worry though, let's get you back on track.
               </p>
@@ -62,7 +62,7 @@ const NotFound = () => {
 
             {/* Additional Info */}
             <div className="mt-6 pt-4 border-t border-slate-200">
-              <p className="text-xs text-slate-700">
+              <p className="text-xs text-slate-100">
                 Error Code: 404 • Requested:{" "}
                 <code className="font-mono text-xs bg-black px-2 py-1 rounded text-white/80">
                   {location.pathname}
