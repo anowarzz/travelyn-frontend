55bfb5208be96fbe50efa32722fede8a51043489
Anowar Hosen <anowarhosen444@gmail.com>
2025-08-13 17:01:39 +0600

    perf: use lazay load for dashboard routes and show a deafult page in dashboard start

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import Login from "@/pages/Login";
 import Register from "@/pages/Register";
 import Verify from "@/pages/Verify";
 import { generateRoutes } from "@/utils/generateRoutes";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 import { adminSidebarItems } from "./adminSidebarItems";
 import { userSidebarItems } from "./userSidebarItems";
 
@@ -23,12 +23,19 @@ const router = createBrowserRouter([
   {
     Component: DashBoardLayout,
     path: "/admin",
-    children: [...generateRoutes(adminSidebarItems)],
+
+    children: [
+      { index: true, element: <Navigate to="/admin/analytics" /> },
+      ...generateRoutes(adminSidebarItems),
+    ],
   },
   {
     Component: DashBoardLayout,
     path: "/user",
-    children: [...generateRoutes(userSidebarItems)],
+    children: [
+      { index: true, element: <Navigate to="/user/bookings" /> },
+      ...generateRoutes(userSidebarItems),
+    ],
   },
   {
     Component: Login,
--- src/routes/userSidebarItems.ts
+++ src/routes/userSidebarItems.ts
@@ -1,5 +1,7 @@
-import Bookings from "@/pages/User/Bookings";
 import type { ISidebarItem } from "@/types";
+import { lazy } from "react";
+
+const Bookings = lazy(() => import("@/pages/User/Bookings"));
 
 export const userSidebarItems: ISidebarItem[] = [
   {
